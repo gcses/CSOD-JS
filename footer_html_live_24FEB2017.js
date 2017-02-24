062f9b0abf77e6613931a75946c96e7b5a82f41a
Corey Decker <corey.decker@bd.com>
2017-02-24 09:14:56 -0500

    add footer script for CSOD pilot and live

---
+++ footer_html_live_24FEB2017.js
@@ -0,0 +1,4 @@
+<div>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://rawgit.com/gcses/csod-js/master/custom.js"></script>
+</div> 
