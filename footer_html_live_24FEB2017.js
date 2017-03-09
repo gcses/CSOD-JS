c72ff266bff729f646490bb76e4f47f61dce82d4
gcses <corey.decker@bd.com>
2017-03-09 08:00:57 -0500

    Remove all code to test browse training bug

--- footer_html_live_24FEB2017.js
+++ footer_html_live_24FEB2017.js
@@ -1,4 +1,1 @@
-<div>
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
-<script type="text/javascript" src="https://rawcdn.githack.com/gcses/CSOD-JS/master/footer_html_live_24FEB2017.js"></script>
-</div> 
+
