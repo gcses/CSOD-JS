399eb181bb22638cf750f0fa4dc594ac4dd016d7
gcses <corey.decker@bd.com>
2017-03-09 09:38:18 -0500

    Add remaining JS changes for testing in pilot

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -13,5 +13,45 @@ $(document).ready(function(){
   $('.navTabs-subTabList a:contains("Browse for Training")').each(function(){
   $(this).html($(this).html().replace('Browse for Training','Browse Learning'));
   });
+  
+  $('.cso-txt span:contains("Online Class")').each(function(){
+ 	$(this).html($(this).html().replace('Online Class','Web-Based Training'));
+	});
+
+
+	$('[data-original-title="Online Class"]').attr('data-original-title', 'Web-Based Training');
+    $('.tooltip-cso').tooltip();
+
+	$('.cso-brdcrm-cont a:contains("Browse for Training")').each(function(){
+ 	$(this).html($(this).html().replace('Browse for Training','Browse Learning'));
+	});
+
+	$('.cso-title-cont.cso-wsp-cont span:contains("Browse for Training")').each(function(){
+ 	$(this).html($(this).html().replace('Browse for Training','Browse Learning'));
+	});
+
+	$('.cso-cont-marb35.cso-cont-mart20 h2:contains("Training")').each(function(){
+ 	$(this).html($(this).html().replace('Training','Learning'));
+	});
+
+	$('.navTabs-subTabList a:contains("Browse for Training")').each(function(){
+ 	$(this).html($(this).html().replace('Browse for Training','Browse Learning'));
+	})
+
+	$('.navTabs-subTabList a:contains("Manage Employee Learning")').each(function(){
+ 	$(this).html($(this).html().replace('Manage Employee Learning','Manage My Learners'));
+	})
+
+	$('.item.oc').css({'background':'url("https://placeholdit.imgix.net/~text?txtsize=33&txt=61×61&w=61&h=61")'});
+
+	$('.item.mt').css({'background':'url("https://placeholdit.imgix.net/~text?txtsize=33&txt=61×61&w=61&h=61")'});
+
+	$('.item.ts').css({'background':'url("https://placeholdit.imgix.net/~text?txtsize=33&txt=61×61&w=61&h=61")'});
+
+	$('.item.ev').css({'background':'url("https://placeholdit.imgix.net/~text?txtsize=33&txt=61×61&w=61&h=61")'});
+
+	$('.item.vd').css({'background':'url("https://placeholdit.imgix.net/~text?txtsize=33&txt=61×61&w=61&h=61")'});
+;
+});
 
 });
