69139e117513ab4e3e69c134ffdcc94069f2a6a8
gcses <corey.decker@bd.com>
2017-03-13 13:01:13 -0700

    Removed double document on load

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -1,25 +1,14 @@
 $(document).ready(function(){
 
   $(".smenu.sm.navTabs").append('<li><a href="javascript:;" title="Support" aria-haspopup="true" aria-expanded="false" aria-controls="menu-Support" class="has-submenu"><span class="sub-arrow">+</span>Support</a><ul class="navTabs-subTabList sm-nowrap" role="group" aria-labelledby="Support" aria-expanded="false" style="display: none; width: auto; min-width: 10em; max-width: 20em; top: auto; left: 0px; margin-left: 0px; margin-top: 0px"><li><a href="http://cp.carefusion.com" title="Change Password" tabindex="-1">Change Password</a></li><li><a href="/catalog/CustomPage.aspx?id=20000480&amp;tab_page_id=20000480" title="Contact Us" tabindex="-1">Contact Us</a></li><li><a href="/catalog/CustomPage.aspx?id=20000480&amp;tab_page_id=20000480" title="Frequently Asked Questions" tabindex="-1">Frequently Asked Questions</a></li></ul></li>');
-
-	$smjquery('#main-menu').smartmenus('refresh')
-
-});
-
-$(document).ready(function(){
-
-	var box = $('#container2 span:contains("No assigned training")');
- 	if(box.length > 0){
-   	box.parents('#container2').children(':contains("No assigned training")').hide();
-	}
+  $smjquery('#main-menu').smartmenus('refresh')
 
 	$('.cso-txt span:contains("Online Class")').each(function(){
  	$(this).html($(this).html().replace('Online Class','Web-Based Training'));
 	});
 
-
 	$('[data-original-title="Online Class"]').attr('data-original-title', 'Web-Based Training');
-        $('.tooltip-cso').tooltip();
+  $('.tooltip-cso').tooltip();
 
 	$('.cso-brdcrm-cont a:contains("Browse for Training")').each(function(){
  	$(this).html($(this).html().replace('Browse for Training','Browse Learning'));
@@ -52,4 +41,5 @@ $(document).ready(function(){
 	$('.item.vd').css({'background':'url("https://placeholdit.imgix.net/~text?txtsize=33&txt=61×61&w=61&h=61")'});
 
 	$('#ctl00_ContentPlaceHolder1_widgetLayout_rptWidgets_ctl01_widgetContainer_ctl00_pnlSuggestedTraining_title').css({'background-color':'#00B8B0'});
+
 });
