9d004f398bdd838c92e146bc84851de30ce2197e
gcses <corey.decker@bd.com>
2017-03-13 13:22:28 -0700

    Removing everything except menu

--- footer_html_live_24FEB2017.js
+++ footer_html_live_24FEB2017.js
@@ -4,45 +4,4 @@ $(document).ready(function(){
     $(".smenu.sm.navTabs").append('<li><a href="javascript:;" title="Support" aria-haspopup="true" aria-expanded="false" aria-controls="menu-Support" class="has-submenu"><span class="sub-arrow">+</span>Support</a><ul class="navTabs-subTabList sm-nowrap" role="group" aria-labelledby="Support" aria-expanded="false" style="display: none; width: auto; min-width: 10em; max-width: 20em; top: auto; left: 0px; margin-left: 0px; margin-top: 0px"><li><a href="http://cp.carefusion.com" title="Change Password" tabindex="-1">Change Password</a></li><li><a href="/catalog/CustomPage.aspx?id=20000480&amp;tab_page_id=20000480" title="Contact Us" tabindex="-1">Contact Us</a></li><li><a href="/catalog/CustomPage.aspx?id=20000480&amp;tab_page_id=20000480" title="Frequently Asked Questions" tabindex="-1">Frequently Asked Questions</a></li></ul></li>');
 
 	$smjquery('#main-menu').smartmenus('refresh')
-
-	var box = $('#container2 span:contains("No assigned training")');
- 	if(box.length > 0){
-   	box.parents('#container2').children(':contains("No assigned training")').hide();
-	}
-
-	$('.cso-txt span:contains("Online Class")').each(function(){
- 	$(this).html($(this).html().replace('Online Class','Web-Based Training'));
-	});
-
-	$('.cso-brdcrm-cont a:contains("Browse for Training")').each(function(){
- 	$(this).html($(this).html().replace('Browse for Training','Browse Learning'));
-	});
-
-	$('.cso-title-cont.cso-wsp-cont span:contains("Browse for Training")').each(function(){
- 	$(this).html($(this).html().replace('Browse for Training','Browse Learning'));
-	});
-
-	$('.cso-cont-marb35.cso-cont-mart20 h2:contains("Training")').each(function(){
- 	$(this).html($(this).html().replace('Training','Learning'));
-	});
-
-	$('.navTabs-subTabList a:contains("Browse for Training")').each(function(){
- 	$(this).html($(this).html().replace('Browse for Training','Browse Learning'));
-	})
-
-	$('.navTabs-subTabList a:contains("Manage Employee Learning")').each(function(){
- 	$(this).html($(this).html().replace('Manage Employee Learning','Manage My Learners'));
-	})
-
-	$('.item.oc').css({'background':'url("https://placeholdit.imgix.net/~text?txtsize=33&txt=61×61&w=61&h=61")'});
-
-	$('.item.mt').css({'background':'url("https://placeholdit.imgix.net/~text?txtsize=33&txt=61×61&w=61&h=61")'});
-
-	$('.item.ts').css({'background':'url("https://placeholdit.imgix.net/~text?txtsize=33&txt=61×61&w=61&h=61")'});
-
-	$('.item.ev').css({'background':'url("https://placeholdit.imgix.net/~text?txtsize=33&txt=61×61&w=61&h=61")'});
-
-	$('.item.vd').css({'background':'url("https://placeholdit.imgix.net/~text?txtsize=33&txt=61×61&w=61&h=61")'});
-
-	$('#ctl00_ContentPlaceHolder1_widgetLayout_rptWidgets_ctl01_widgetContainer_ctl00_pnlSuggestedTraining_title').css({'background-color':'#00B8B0'});
 });
