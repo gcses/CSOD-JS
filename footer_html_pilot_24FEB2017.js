4a8c2f74f74c1152650c23f4279dc7843aa7943b
gcses <john.santucci@carefusion.com>
2017-03-23 15:08:50 -0700

    0DollarEmpty2ndTry
    Empty span containing $0.00

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	
 	$("span:contains('$0.00')").each(function(){
- 		$(this).closest('h3').empty();
+ 		$(this).empty();
 	});
 	
 	$("[aria-labelledby=Support]").empty();
