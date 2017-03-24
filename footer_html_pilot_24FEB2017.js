3c7163b086dcd21678ee18a9aeb033ccbbaa0149
gcses <john.santucci@carefusion.com>
2017-03-24 13:54:36 -0700

    windowOn

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
+//$(document).ready(function(){
+ $(window).on('load', function () {
 	
 	$("h2:contains('Subject')").each(function(){
  		$(this).html($(this).html().replace('Subject','Topics'));
