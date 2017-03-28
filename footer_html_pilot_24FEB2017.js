0c07683faf32e53297c87719f183e969c2fe348b
gcses <john.santucci@carefusion.com>
2017-03-28 08:33:40 -0700

    shorterDelayOnlineReplace

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -83,7 +83,7 @@ $(document).ready(function(){
 
 	$('#ctl00_ContentPlaceHolder1_widgetLayout_rptWidgets_ctl01_widgetContainer_ctl00_pnlSuggestedTraining_title').css({'background-color':'#00B8B0'});
 
-	window.setInterval(replaceOnlineClass, 10000);
+	window.setInterval(replaceOnlineClass, 1000);
 
 	function replaceOnlineClass() {
 		
