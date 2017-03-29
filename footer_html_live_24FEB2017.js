845ee7fe56a11eee1f2a6c6e2ab563c1d1264c9b
gcses <john.santucci@carefusion.com>
2017-03-29 14:49:52 -0700

    Update footer_html_live_24FEB2017.js

--- footer_html_live_24FEB2017.js
+++ footer_html_live_24FEB2017.js
@@ -4,10 +4,10 @@ $(document).ready(function(){
    //$name = $("{FIRSTNAME}");
   // $name = {FIRSTNAME};
 		
-   var $name = $({FIRSTNAME});
+   var $name = {FIRSTNAME};
 	
    $('h2:contains("My Learning")').each(function(){
- 	    $(this).html($(this).html().replace('My Learning', $name + '\'s Learning In Progress'));
+ 	    $(this).html($(this).html().replace('My Learning', $name.value + '\'s Learning In Progress'));
 	});
 	
    $(".smenu.sm.navTabs").append('<li><a href="javascript:;" title="Support" aria-haspopup="true" aria-expanded="false" aria-controls="menu-Support" class="has-submenu"><span class="sub-arrow">+</span>Support</a><ul class="navTabs-subTabList sm-nowrap" role="group" aria-labelledby="Support" aria-expanded="false" style="display: none; width: auto; min-width: 10em; max-width: 20em; top: auto; left: 0px; margin-left: 0px; margin-top: 0px"><li><a href="http://cp.carefusion.com" title="Change Password" tabindex="-1">Change Password</a></li><li><a href="/catalog/CustomPage.aspx?id=20000480&amp;tab_page_id=20000480" title="Contact Us" tabindex="-1">Contact Us</a></li><li><a href="/catalog/CustomPage.aspx?id=20000480&amp;tab_page_id=20000480" title="Frequently Asked Questions" tabindex="-1">Frequently Asked Questions</a></li></ul></li>');
