934f42cf4ba7718431a2fe47079a78772307b494
gcses <john.santucci@carefusion.com>
2017-03-30 16:10:43 -0700

    Update footer_html_live_24FEB2017.js

--- footer_html_live_24FEB2017.js
+++ footer_html_live_24FEB2017.js
@@ -1,6 +1,9 @@
 
 $(document).ready(function(){
-   
+	
+   //show the advanced global search options by default
+   $('.divAdvanceControls').show();
+	
   //hide the first name field. It is only included to that we can use the name elsewhere
   $("#ctl00_ContentPlaceHolder1_widgetLayout_rptWidgets_ctl01_widgetContainer_ctl00_htmlContainer").hide();
 	
