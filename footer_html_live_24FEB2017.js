f4ae2b78b2cec1537695527aadd6c4039ff7ba90
gcses <john.santucci@carefusion.com>
2017-03-30 16:26:37 -0700

    Update footer_html_live_24FEB2017.js

--- footer_html_live_24FEB2017.js
+++ footer_html_live_24FEB2017.js
@@ -2,7 +2,8 @@
 $(document).ready(function(){
 	
    //show the advanced global search options by default
-   globalSearchManager.showAdvanceOptionIFrame();
+   $("#ctl00_ContentPlaceHolder1_ucSearchAdvanceControls_hfCurrentSearchEngine").show();
+   $("ctl00_ContentPlaceHolder1_ucSearchAdvanceControls_hfAdvanceOptions").show();
 	
   //hide the first name field. It is only included to that we can use the name elsewhere
   $("#ctl00_ContentPlaceHolder1_widgetLayout_rptWidgets_ctl01_widgetContainer_ctl00_htmlContainer").hide();
