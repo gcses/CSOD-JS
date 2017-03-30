f59584197fb578e5566a247a1a6e58488b249f42
gcses <john.santucci@carefusion.com>
2017-03-29 17:28:24 -0700

    Update footer_html_live_24FEB2017.js

--- footer_html_live_24FEB2017.js
+++ footer_html_live_24FEB2017.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
    
    //$name = $("{FIRSTNAME}");
 			
-  var names = $("#ctl00_ContentPlaceHolder1_widgetLayout_rptWidgets_ctl00_widgetContainer_ctl00_htmlContainer").find( "p:last" ).text().split(' ');
+  var names = $("#ctl00_ContentPlaceHolder1_widgetLayout_rptWidgets_ctl00_widgetContainer_ctl00_htmlContainer").find( "p:last" ).text().split(" ");
   var fName = names.length;	
   $('h2:contains("My Learning")').each(function(){
  	    $(this).html($(this).html().replace('My Learning', fName + '\'s Learning In Progress'));
