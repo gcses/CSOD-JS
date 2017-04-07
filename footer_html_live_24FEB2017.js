5a4b4a8affb1bbdf98acdce3dce5e975f8a7bb3a
gcses <john.santucci@carefusion.com>
2017-04-07 11:07:40 -0700

    Update footer_html_live_24FEB2017.js

--- footer_html_live_24FEB2017.js
+++ footer_html_live_24FEB2017.js
@@ -192,32 +192,49 @@ $(document).ready(function(){
         // $('.cso-h1').css("color", "red");		
  
 	
-
-		
-  //show the advanced search option whenever search is shown
-  $("#lnkShowMoreSearchOptions").each(function(){
-      //$("#srch-ctrl").each(function(){
-	// console.log("#srch-ctrl.offsetHeight = " + $(this).offsetHeight);
-	//});
-	eventFire(document.getElementById('lnkShowMoreSearchOptions'), 'click');
-   });
+       //show the advanced search option whenever search is shown
+       $("#lnkShowMoreSearchOptions").each(function(){
+        	eventFire(document.getElementById('lnkShowMoreSearchOptions'), 'click');
+       });
 	
-  //remove the close button on the advanced options
-  $(".cs-advsrch-close-btn").remove();
+       //remove the close button on the advanced options
+       $(".cs-advsrch-close-btn").remove();
 	
-  //hide the first name field. It is only included to that we can use the name elsewhere
-  $("#ctl00_ContentPlaceHolder1_widgetLayout_rptWidgets_ctl01_widgetContainer_ctl00_htmlContainer").hide();
+       //hide the first name field. It is only included to that we can use the name elsewhere
+       $("#ctl00_ContentPlaceHolder1_widgetLayout_rptWidgets_ctl01_widgetContainer_ctl00_htmlContainer").hide();
 	
-  //use the name field to alter the My Learning header
-  var fName = $("#ctl00_ContentPlaceHolder1_widgetLayout_rptWidgets_ctl01_widgetContainer_ctl00_htmlContainer").text();
-  $('h2:contains("My Learning")').each(function(){
+       //use the name field to alter the My Learning header
+       var fName = $("#ctl00_ContentPlaceHolder1_widgetLayout_rptWidgets_ctl01_widgetContainer_ctl00_htmlContainer").text();
+       $('h2:contains("My Learning")').each(function(){
  	    $(this).html($(this).html().replace('My Learning', fName + '\'s Learning In Progress'));
 	});
 	
-   //add smart menu items that allow for third party links
-   $(".smenu.sm.navTabs").append('<li><a href="javascript:;" title="Support" aria-haspopup="true" aria-expanded="false" aria-controls="menu-Support" class="has-submenu"><span class="sub-arrow">+</span>Support</a><ul class="navTabs-subTabList sm-nowrap" role="group" aria-labelledby="Support" aria-expanded="false" style="display: none; width: auto; min-width: 10em; max-width: 20em; top: auto; left: 0px; margin-left: 0px; margin-top: 0px"><li><a href="http://cp.carefusion.com" title="Change Password" tabindex="-1">Change Password</a></li><li><a href="/catalog/CustomPage.aspx?id=20000480&amp;tab_page_id=20000480" title="Contact Us" tabindex="-1">Contact Us</a></li><li><a href="/catalog/CustomPage.aspx?id=20000480&amp;tab_page_id=20000480" title="Frequently Asked Questions" tabindex="-1">Frequently Asked Questions</a></li></ul></li>');
+       //add smart menu items that allow for third party links
+       $(".smenu.sm.navTabs").append('<li><a href="javascript:;" title="Support" aria-haspopup="true" aria-expanded="false" aria-controls="menu-Support" class="has-submenu"><span class="sub-arrow">+</span>Support</a><ul class="navTabs-subTabList sm-nowrap" role="group" aria-labelledby="Support" aria-expanded="false" style="display: none; width: auto; min-width: 10em; max-width: 20em; top: auto; left: 0px; margin-left: 0px; margin-top: 0px"><li><a href="http://cp.carefusion.com" title="Change Password" tabindex="-1">Change Password</a></li><li><a href="/catalog/CustomPage.aspx?id=20000480&amp;tab_page_id=20000480" title="Contact Us" tabindex="-1">Contact Us</a></li><li><a href="/catalog/CustomPage.aspx?id=20000480&amp;tab_page_id=20000480" title="Frequently Asked Questions" tabindex="-1">Frequently Asked Questions</a></li></ul></li>');
 
-   $smjquery('#main-menu').smartmenus('refresh');
+        $smjquery('#main-menu').smartmenus('refresh');
+	
+        $("a:has(span:contains('Popular'))").remove();
+	
+        $('h2:contains("Browse for Training")').each(function(){
+ 		$(this).html($(this).html().replace('Browse for Training','Browse Learning'));
+	});
+	
+	$('h2:contains("Suggested Training")').each(function(){
+ 		$(this).html($(this).html().replace('Suggested Training','Suggested Learning'));
+	});
+	
+	$('a:contains("Change your password")').each(function(){
+ 		$(this).remove();
+	});
+	
+	$('a:contains("Go to Customer Portal")').each(function(){
+ 		$(this).remove();
+	});
+	
+	
+	
+	
 });
 
 
