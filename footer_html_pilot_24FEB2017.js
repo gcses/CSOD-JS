5fad76b389c05e1fb0bd44e57e229e01c9a3ec2c
gcses <john.santucci@carefusion.com>
2017-05-17 12:37:22 -0700

    Update footer_html_pilot_24FEB2017.js

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
  
-	console.log("Test4");
+	console.log("Test5");
 	
 	//hide the various Cornerstone footers
 	$('.df-footer').hide();
@@ -53,8 +53,10 @@ $(document).ready(function(){
 	//$("#my-slider").css({"margin": "-90px 0px 0px 0px"});  
 	
 	$("#my-slider").css({"width": "100%", "margin": "0px 0px 0px 0px",  }); 
-	$("#ctl00_ContentPlaceHolder1_widgetLayout_rptWidgets_ctl00_widgetContainer_ctl00_htmlContainer").css({"width": "100%", "margin": "-66px 0px 0px 0px" }); 
+	$("#ctl00_ContentPlaceHolder1_widgetLayout_rptWidgets_ctl00_widgetContainer_ctl00_htmlContainer").css({"width": "100%", "margin": "-30px 0px 0px 0px" }); 
 	$("#ctl00_masterPageBody").css({"width": "100%", "margin": "-66px 0px 0px 0px" }); 
+	
+	
 	$("#ctl00_ContentPlaceHolder1_labelError").css({"width": "100%"}); 
 	$("#ctl00_ContentPlaceHolder1_widgetLayout_mainPageDiv").css({"width": "100%"}); 
 	$("#ctl00_ContentPlaceHolder1_widgetLayout_mainDivRenderedWidgets").css({"width": "100%"}); 
