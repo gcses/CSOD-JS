922b64f666e9bbf09f0597642cfc508e8c7e72cf
gcses <john.santucci@carefusion.com>
2017-05-23 14:21:25 -0700

    Update footer_html_live_24FEB2017.js

--- footer_html_live_24FEB2017.js
+++ footer_html_live_24FEB2017.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
      //$('.cso-h1').css("color", "red");
 	
-	console.log("Test3");
+	console.log("Test4");
 	
 	$('#main-menu').css({"background-image": "none", "background-color": "#004593"});
 	
@@ -16,7 +16,7 @@ $(document).ready(function(){
 	}
 	
 	//move enlarge slider text / link
-	 $(".ism-caption ism-caption-0").css({"width": "301px"}); 
+	 $(".ism-caption ism-caption-0").css({"width": "501px", "height": "70px"}); 
 	
 	
 	
