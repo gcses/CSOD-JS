6eb6578d80b421f8f32ab1bc1d1d2112b5c2fc35
gcses <john.santucci@carefusion.com>
2017-05-24 13:46:13 -0700

    Update footer_html_pilot_24FEB2017.js

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	console.log("Test2");
+	console.log("Test3");
 	
 	//change the navigation bar to white, remove gradient		
 	$('#main-menu').css({"background-image": "none", "background-color": "#ffffff"});
@@ -92,19 +92,22 @@ $(document).ready(function(){
 
 	    /*Tables*/
             $('.cs-panel-flat, .cs-panel-plain, .cs-panel-gradient').css({"border-radius": "0"});
- 	    $('.cs-panel-flat .Panel_content').css({"border-color": "transparent"});
+ 	   // $('.cs-panel-flat .Panel_content').css({"border-color": "transparent"});
 		
 	    $('.cs-panel-flat .Panel_title').css({"background-color": "#ffffff"});
+		
 	    $('.Panel_titleMiddle a').css({"color": "#000000"}); 
-	    $(".Panel_titleMiddle a").hover(function(){ 
-		 $(this).css({"color": "#000000"}); },
-		 function() { $(this).css({"color": ""}); }
-		); 
 		
+		//what do we want this hover to do?
+	   // $(".Panel_titleMiddle a").hover(function(){ 
+	//	 $(this).css({"color": "#000000"}); },
+		// function() { $(this).css({"color": ""}); }
+	//	); 
 		
+	    $('.cs-panel-flat.Panel_titleMiddle div, .cs-panel-plain.Panel_titleMiddle div, .cs-panel-gradient.Panel_titleMiddle div, .cs-panel-flat.Panel_titleMiddle h2, .cs-panel-plain.Panel_titleMiddle h2, .cs-panel-gradient.Panel_titleMiddle h2').css({"font-size": "20px!important", "font-weight": "lighter"});
+	
 		
-	  //  $('.cs-panel-flat .Panel_titleMiddle div, .cs-panel-plain .Panel_titleMiddle div, .cs-panel-gradient .Panel_titleMiddle div, .cs-panel-flat .Panel_titleMiddle h2, .cs-panel-plain .Panel_titleMiddle h2, .cs-panel-gradient .Panel_titleMiddle h2').css({"
-	  //  $('.cs-panel-flat').css({"
+		//  $('.cs-panel-flat').css({"
           //  $('table .CsList th').css({"
 	  //  $('.Panel_contentMiddle td').css({"
 	  //  $('.Panel_contentMiddle tr:hover, .tablerolloverNewUI,.CsList tbody tr:hover td').css({"
