df999c63e259d5b7748b26f258ba18c29fc0bbe9
gcses <john.santucci@carefusion.com>
2017-05-25 16:11:13 -0700

    Update footer_html_live_24FEB2017.js

--- footer_html_live_24FEB2017.js
+++ footer_html_live_24FEB2017.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
      //$('.cso-h1').css("color", "red");
 	
-	console.log("Test1");
+	console.log("Test2");
 	
 	$('#main-menu').css({"background-image": "none", "background-color": "#004593"});
 	
@@ -34,7 +34,8 @@ $(document).ready(function(){
 		
 		
 		//remove rounded corners from slider
-           	$("#my-slider").css({"width": "101%", "margin-bottom": "200px"});  
+           	//$("#my-slider").css({"width": "101%", "margin-bottom": "200px"});  
+		$("#my-slider").css({"width": "70%", "margin-bottom": "200px"}); 
 	   	document.getElementById("my-slider").style.borderRadius = "0px";
 	
 	  	// //Resize and reposition slider - margin: top, right, bottom, left
