f1e8d1fcf59cb89f8ddb468f5f4a89cb69449cd0
gcses <john.santucci@carefusion.com>
2017-05-25 16:25:03 -0700

    Update footer_html_live_24FEB2017.js

--- footer_html_live_24FEB2017.js
+++ footer_html_live_24FEB2017.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
      //$('.cso-h1').css("color", "red");
 	
-	console.log("Test2");
+	console.log("Test3");
 	
 	$('#main-menu').css({"background-image": "none", "background-color": "#004593"});
 	
@@ -35,13 +35,14 @@ $(document).ready(function(){
 		
 		//remove rounded corners from slider
            	//$("#my-slider").css({"width": "101%", "margin-bottom": "200px"});  
-		$("#my-slider").css({"width": "70%", "margin-bottom": "180px"}); 
+		$("#my-slider").css({"width": "70%", "margin-bottom": "250px"}); 
 	   	document.getElementById("my-slider").style.borderRadius = "0px";
 	
 	  	// //Resize and reposition slider - margin: top, right, bottom, left
 	  	//$("#ctl00_ContentPlaceHolder1_widgetLayout_rptWidgets_ctl00_widgetContainer_ctl00_htmlContainer").css({"width": "70%", "margin": "0px 00% 0px 0px"}); 
-	   	//$("#ctl00_masterPageBody").css({"width": "70%", "margin": "0px 0px 0px 0px" });  //this is the frame and shadow around slider
-	   	//$("#ctl00_ContentPlaceHolder1_labelError").css({"width": "100%"}); 
+	   	 $("#ctl00_masterPageBody").css({"width": "70%", "margin": "0px 0px 0px 0px" });  //this is the frame and shadow around slider
+	   	
+		//$("#ctl00_ContentPlaceHolder1_labelError").css({"width": "100%"}); 
 	   	//$("#ctl00_ContentPlaceHolder1_widgetLayout_mainPageDiv").css({"width": "100%"}); 
 	   	//$("#ctl00_ContentPlaceHolder1_widgetLayout_mainDivRenderedWidgets").css({"width": "100%"}); 
 	   	//$("#mainContainer").css({"width": "100%"}); 
