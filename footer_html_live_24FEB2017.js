18e53727e6a20f2ad0f00590612ad4133fa62a41
gcses <john.santucci@carefusion.com>
2017-05-25 10:36:26 -0700

    Update footer_html_live_24FEB2017.js

--- footer_html_live_24FEB2017.js
+++ footer_html_live_24FEB2017.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
      //$('.cso-h1').css("color", "red");
 	
-	console.log("Test5");
+	console.log("Test6");
 	
 	$('#main-menu').css({"background-image": "none", "background-color": "#004593"});
 	
@@ -23,10 +23,13 @@ $(document).ready(function(){
 	if (document.title.indexOf("Welcome - Realize Your Potential") > -1)
 	{
 	    //add action button to slider
-	    $("#sliderButton").css({"opacity": "0.8", "font-weight": "bold", "padding": "12px", "width": "90px", "height": "15px", "color": "#ffffff", "appearance": "button", "text-decoration": "none", "backgroundColor": "#004593", "margin": "85px 0px 0px 40%"}); 
-	    var element = document.getElementById("sliderButton");
-            element.style.position = "absolute";
-            element.style.zIndex="2147483620";
+	    $(".sliderButton").css({"opacity": "0.8", "font-weight": "bold", "padding": "12px", "width": "90px", "height": "15px", "color": "#ffffff", "appearance": "button", "text-decoration": "none", "backgroundColor": "#004593", "margin": "85px 0px 0px 40%"}); 
+	    var elements = document.getElementsByClassName("sliderButton");
+	    for (var i = 0; i < elements.length; i++) 
+	    {
+		 elements[i].style.position = "absolute";
+           	 elements[i].style.zIndex="2147483620";
+	     }
 	}
 	
 	//Make the home page clickable and link to the Welcome page / Main page
