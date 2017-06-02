f6c959df8c4881c58e90332028c1a3338ecc2299
gcses <john.santucci@carefusion.com>
2017-06-02 14:40:17 -0700

    Update footer_html_pilot_24FEB2017.js

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -24,13 +24,24 @@ $(window).scroll(function() {
 
 $(document).ready(function() {
 	
-	console.log("Test6");
+	console.log("Test7");
 	
 	$('#ctl00_footer_pnlCustomFooter').css({"margin-bottom": "-15px", "backgroundColor": "#f2f2f2", "width": "100%", "padding": "0px"});
 	$('.df-footer').css({"margin": "0px", "backgroundColor": "#f2f2f2", "width": "100%", "padding": "0px"});					
 						
 	$('.df-footer').each(function(){
-	        $(this).html('<img src="https://bd-pilot.csod.com/clientimg/bd/welcome/angle.png" id="slopedFooterImage" /><table class="footerTable"><tr><td id="footerTD1"><img src="https://bd-pilot.csod.com/clientimg/bd/welcome/logo.png" id="footerLogo"></td>  <td id="footerTD2"></td>  <td id="footerTD3" ></td>  <td id="footerTD4"></td>   <td id="footerTD5"> <p class="copy">&copy; 2017 BD. All rights reserved. Unless otherwise noted, BD, the BD Logo and all other trademarks are property of Becton, Dickinson and Company.</p></td>    <td id="footerTD6"></td>  <td id="footerTD7"></td>   <td id="footerTD8"></td> </tr></table>');
+	        $(this).html('<img src="https://bd-pilot.csod.com/clientimg/bd/welcome/angle.png" id="slopedFooterImage" />' +
+			     '<table class="footerTable"><tr><td id="footerTD1"><img src="https://bd-pilot.csod.com/clientimg/bd/welcome/logo.png" id="footerLogo"></td>' +
+			     '<td id="footerTD2"></td>  <td id="footerTD3" ></td>  <td id="footerTD4"></td>' +
+			     '<td id="footerTD5"> <p class="copy">&copy; 2017 BD. All rights reserved. Unless otherwise noted, BD, the BD Logo and all other trademarks are property of Becton, Dickinson and Company.</p></td>'+
+			     '<td id="footerTD6"></td>  <td id="footerTD7"></td>'+
+			     '<td id="footerTD8"><ul class="footer-links">'+
+            			'<li><a href="#">BD Worldwide</a></li>
+           			 '<li><a href="#">Contact BD</a></li>'+
+            			'<li><a href="#">Privacy Policy</a></li>'+
+            			'<li><a href="#">Terms of Use</a></li>'+
+           			 '<li><a href="#">Permission Request</a></li>'+
+        			'</ul></td> </tr></table>')
 		});
 	
 	 $('#slopedFooterImage').css({"width": "100%", "padding-bottom": "0px", "margin-bottom": "-5px"});
