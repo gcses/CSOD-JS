536ee4ad28c79738ad072181a7ab800bf5b61117
gcses <john.santucci@carefusion.com>
2017-06-03 13:53:45 -0700

    Update footer_html_pilot_24FEB2017.js

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
 	
 	console.log("Test6");
 	
-	$('#ctl00_footer_pnlCustomFooter').css({"margin-bottom": "-15px", "backgroundColor": "#f2f2f2", "width": "100%", "padding": "0px"});
+	$('#ctl00_footer_pnlCustomFooter').css({"margin-top": "15px", "margin-bottom": "-15px", "backgroundColor": "#f2f2f2", "width": "100%", "padding": "0px"});
 	$('.df-footer').css({"margin": "0px", "backgroundColor": "#f2f2f2", "width": "100%", "padding": "0px"});					
 						
 	$('.df-footer').each(function(){
@@ -54,11 +54,9 @@ $(document).ready(function() {
 	$('.footer-links li:first-child').css({"border-left": "none"});
 	$('.footer-links a').css({"color": "#FFFFFF", "font-weight": "400", "text-decoration": "none", "font-size": "10px"});
 	
-	
 	$('.social-media-links').css({"padding-left": "0", "float": "right", "padding-right": "15px", "margin-top": "8px"});
 	$('.social-media-links li').css({"display": "inline-block", "line-height": "16px", "padding-right": "5px", "padding-left": "0px", "border": "none"});
 	$('.social-media-links a').css({"text-decoration": "none"});
-	
 	
 	 $('#slopedFooterImage').css({"width": "100%", "padding-bottom": "0px", "margin-bottom": "-5px"});
 	
