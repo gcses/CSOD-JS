a97634e8173085ef455ad0d72ee063140b2fee53
gcses <john.santucci@carefusion.com>
2017-06-14 14:55:33 -0700

    Create footer_html_pilot_24FEB2017.js

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -24,7 +24,7 @@ $(window).scroll(function() {
 
 $(document).ready(function() {
 
-	console.log("Test4");
+	console.log("Test5");
 	
 	$('#ctl00_footer_pnlCustomFooter').css({"margin-top": "0px", "margin-bottom": "-15px", "backgroundColor": "#f2f2f2", "width": "100%", "padding": "0px"});
 	
@@ -605,15 +605,15 @@ function createAccordionControls()
     //add up and down arrows to the main accordion items
 
 	    var upArrowImg = $('<img class="upArrowIcon">');     //Equivalent: $(document.createElement('img'))
-	    chevImg.attr('src', 'https://bd-pilot.csod.com/clientimg/bd/welcome/upArrow3.jpg');
-	    chevImg.attr('height','12px');
-           // chevImg.appendTo('.cs-subjwidg-list li.cat a');
+	    upArrowImg.attr('src', 'https://bd-pilot.csod.com/clientimg/bd/welcome/upArrow3.jpg');
+	    upArrowImg.attr('height','12px');
+           // upArrowImg.appendTo('.cs-subjwidg-list li.cat a');
 	
 	
 	   var downArrowImg = $('<img class="downArrowIcon">');     //Equivalent: $(document.createElement('img'))
-	    chevImg.attr('src', 'https://bd-pilot.csod.com/clientimg/bd/welcome/downArrow3.jpg');
-	    chevImg.attr('height','12px');
-           // chevImg.appendTo('.cs-subjwidg-list li.cat a');
+	    downArrowImg.attr('src', 'https://bd-pilot.csod.com/clientimg/bd/welcome/downArrow3.jpg');
+	    downArrowImg.attr('height','12px');
+           // downArrowImg.appendTo('.cs-subjwidg-list li.cat a');
 	
 	
 	   // $('.chevron1').css({'position': 'absolute', 'top': '13%', 'left': '81%', 'margin-left': '5px'});
