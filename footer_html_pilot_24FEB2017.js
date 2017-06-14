cabc831209a4a56728bb5284ded8ade135520f81
gcses <john.santucci@carefusion.com>
2017-06-14 16:50:03 -0700

    Create footer_html_pilot_24FEB2017.js

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -24,7 +24,7 @@ $(window).scroll(function() {
 
 $(document).ready(function() {
 
-	console.log("Test4");
+	console.log("Test5");
 	
 	$('#ctl00_footer_pnlCustomFooter').css({"margin-top": "0px", "margin-bottom": "-15px", "backgroundColor": "#f2f2f2", "width": "100%", "padding": "0px"});
 	
@@ -607,11 +607,11 @@ function createAccordionControls()
    // $('.cs-subjwidg-list li.cat.nochild .exp-icon').css({'background-image': 'url("https://bd-pilot.csod.com/clientimg/bd/welcome/downArrow3.jpg")'});
    // $('#dir-rtl .cs-subjwidg-list li.cat .exp-icon').css({'background-image': 'url("https://bd-pilot.csod.com/clientimg/bd/welcome/downArrow3.jpg")'});
 	
-    $(".cs-subjwidg-list li.cat a.exp-icon:after").css({'background-image': 'https://bd-pilot.csod.com/clientimg/bd/welcome/downArrow3.jpg'});  
+    $(".cs-subjwidg-list li.cat a.exp-icon:after").css({'background-image': 'url("https://bd-pilot.csod.com/clientimg/bd/welcome/downArrow3.jpg")'});  
     $(".cs-subjwidg-list li.cat.nochild .exp-icon:after").css({"background-image": "none"});  
    
      //replace minus with up arrow
-     $('#dir-rtl .cs-subjwidg-list li.cat .exp-icon.selected').css({'background-image': 'https://bd-pilot.csod.com/clientimg/bd/welcome/upArrow3.jpg'});
+     $('#dir-rtl .cs-subjwidg-list li.cat .exp-icon.selected').css({'background-image': 'url("https://bd-pilot.csod.com/clientimg/bd/welcome/upArrow3.jpg")'});
 	
 	
 	
