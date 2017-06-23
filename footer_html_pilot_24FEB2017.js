1b1df933ce27b033fe8c4a3486951a3e0c9686de
gcses <john.santucci@carefusion.com>
2017-06-22 20:05:13 -0700

    Update footer_html_pilot_24FEB2017.js

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -34,22 +34,11 @@ $(document).ready(function() {
 	cogImg.attr('src', 'https://bd-pilot.csod.com/clientimg/bd/welcome/cog.png');
 	cogImg.appendTo('#optionsbtn');
 
+	
 	$('.cog1').css({'height': '30px', 'position': 'absolute', 'top': '5%', 'left': '5%', 'border': '6px', "zindex": "2147483644"});
 	$('#optionsbtn').css({'backgroundColor': '#004593', 'border': '6px', "zindex": "2147483644"});
-	
-
-	//$('.settings span').css({'backgroundColor': '#004593'});
-	//$('.settings').css({'backgroundColor': '#004593',  'border': '6px solid yellow'});
-	//$('.item').css({'backgroundColor': '#004593',  'border': '6px solid yellow'});
-	//$('.cso-hdr-dd').css({'backgroundColor': '#004593',  'border': '6px solid yellow'});
-	//$('#header_headerFixed_imgAvatar').css({'backgroundColor': '#004593',  'border': '6px solid yellow'});
-	
-	
-	//$('.user-interaction').css({'backgroundColor': '#004593'});
-	//$('.user-interaction span').css({'backgroundColor': '#004593'});
-	$('#header_headerFixed_hlAvatar').css({'backgroundColor': '#004593',  'border': '6px solid yellow'});
+	$('#header_headerFixed_hlAvatar').css({'backgroundColor': '#004593',  'border': '6px solid #004593'});  //yellow border
 	$('.avatar').css({'backgroundColor': '#004593',  'border': '6px solid'});
-	
 	
 	
         // Profile  
@@ -63,12 +52,7 @@ $(document).ready(function() {
             elementAvatar.style.zIndex="2147483644";
 		
 	    // margin: top, right, bottom, left
-	    $('#header_headerFixed_imgAvatar').css({'width': '35px', 'margin': '-5px 0px 0px -20px', 'border-left': '6px ' , 'border-right': '6px', 'padding-left': '10px', 'padding-right': '10px'});
-		
-			
-		
-
-
+	    $('#header_headerFixed_imgAvatar').css({'width': '35px', 'margin': '-5px 0px 0px -30px', 'border-left': '6px ' , 'border-right': '6px', 'padding-left': '10px', 'padding-right': '10px'});
 	}
 	else
 	{
