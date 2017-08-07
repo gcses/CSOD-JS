9b7d611443094d29dce791e9a3a84b1f67408865
gcses <john.santucci@carefusion.com>
2017-08-07 16:37:03 -0700

    Update footer_html_pilot_24FEB2017.js

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -345,7 +345,8 @@ $(document).ready(function() {
      //adjust slider size
         //$("#my-slider").css({"width": "101%", "margin-bottom": "200//px"}); 
        // margin bottom pushes down the Cards
-	$("#my-slider").css({"border": "none", "width": "100%", "margin-bottom": "10px", "margin-top": "200px", "padding-bottom": "0px", "padding-top": "0px"}); 
+	$("#my-slider").css({"border": "none", "width": "100%", "margin-bottom": "10px", "margin-top": "0px",
+			     "padding-bottom": "0px", "padding-top": "0px"}); 
 	
 	//push down the My Learninig box	
         $("#ctl00_ContentPlaceHolder1_widgetLayout_rptWidgets_ctl04_widgetContainer_ctl00_pnlMyTraining").css({"margin-top": "150px"}); 
@@ -355,6 +356,16 @@ $(document).ready(function() {
 		
       //bring entire slider and body in general up to be flush with the warning banner under the nav bar
        $("#ctl00_masterPageBody").css({ "margin": "0px 0px 0px 0px"}); 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	
       //Resize and reposition slider - margin: top, right, bottom, left
 	  //$("#ctl00_ContentPlaceHolder1_widgetLayout_rptWidgets_ctl00_widgetContainer_ctl00_htmlContainer").css({"width": "102%", "margin": "0px 0px 0px -5px"}); 
@@ -382,7 +393,8 @@ $(document).ready(function() {
 		$(".widgetToMove").css({"box-shadow": "0 1px 6px 0 rgba(0,0,0,0.2)", "margin": "250px 0px 0px 0px"});
 	        $(".card").css({"width": "20%", "box-sizing": "border-box", "padding": "0px", "border-radius": "0px 0px 0px 0px",
 				"border-top": "solid 9px #00a79e", "border-bottom": "none", "border-left": "solid 1px #f2f3f4", 
-				"border-right": "none", "margin": "250px 0px 0px 0px" }); 
+				"border-right": "none", "margin": "0px 0px 0px 0px" }); 
+		
 		$(".card:first-child").css({"border-left": "none"});
 		$(".card:nth-child(2)").css({"border-top": "solid 9px #6f2780"});
 		$(".card:nth-child(3)").css({"border-top": "solid 9px #02abd7"});
