73db417f6fbd93637ae24609ee2de18ef441cabb
gcses <john.santucci@carefusion.com>
2017-10-13 09:38:26 -0700

    Update footer_html_pilot_24FEB2017.js

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -24,7 +24,7 @@ $(window).scroll(function() {
 
 $(document).ready(function() {
 
-	console.log("Test4");
+	console.log("Test1");
 
 	//Alternative Header Icons
 
@@ -215,7 +215,7 @@ $(document).ready(function() {
 	if (document.title.indexOf("Welcome - Realize Your Potential") > -1)
 	{
 	      //change slider's slide title text
-	      $('.ism-caption.ism-caption-0').css({'height': '75px', 'backgroundColor': 'yellow', 'color': 'white', 'font-size': '300%'});
+	      $('.ism-caption.ism-caption-0').css({'margin': '0px 0px 0px 45%', 'height': '75px', 'backgroundColor': 'yellow', 'color': 'white', 'font-size': '300%'});
 	      $('#my-slider li').css({'font-size': '300%', 'backgroundColor': 'yellow'});
 		
 		
