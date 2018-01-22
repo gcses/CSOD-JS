8e547130722af999d5a614973a1639d5b865ff76
gcses <john.santucci@carefusion.com>
2018-01-22 10:00:29 -0800

    Update footer_html_pilot_24FEB2017.js

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -1,5 +1,5 @@
 (function($) {
-    console.log("Hello World");
+    console.log("Hello World 1");
    
 
         $('.topBar').css({'backgroundColor': '#004593',  'margin-top': '0px', 'margin-right': '10px', 'border': '0px solid #004593'});  
