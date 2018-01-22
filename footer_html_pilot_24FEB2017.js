2bab49d5d81ef3613a68a135c769e072129b7cf7
gcses <john.santucci@carefusion.com>
2018-01-22 12:49:14 -0800

    Update footer_html_pilot_24FEB2017.js

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -1,10 +1,11 @@
-jQuery(function($) {
+(function($) {
     console.log("Hello World 2");
     if (typeof jQuery == 'undefined') {
         var script = document.createElement('script');
         script.type = "text/javascript";
         script.src = "https://code.jquery.com/jquery-3.3.1.min.js";
         document.getElementsByTagName('head')[0].appendChild(script);
+        
         console.log("Injected Jquery to Head");
     }
 
