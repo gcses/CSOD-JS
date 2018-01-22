fdd83f0c6e2df6dbf5b7f6ee2c453f7bfd895709
gcses <john.santucci@carefusion.com>
2018-01-22 10:06:44 -0800

    Update footer_html_pilot_24FEB2017.js

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -1,8 +1,12 @@
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+
 (function($) {
     console.log("Hello World 1");
-   
-
-        $('.topBar').css({'backgroundColor': '#004593',  'margin-top': '0px', 'margin-right': '10px', 'border': '0px solid #004593'});  
-   
-  
+    $( document ).ready(function() {
+        console.log( "ready!" );
+           $('.topBar').css({'backgroundColor': '#004593',  'margin-top': '0px', 'margin-right': '10px', 'border': '0px solid #004593'});  
+    });
 })(jQuery);
