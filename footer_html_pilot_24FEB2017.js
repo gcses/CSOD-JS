3af652b352beed43339cc8495cc5a9663905172d
gcses <john.santucci@carefusion.com>
2018-01-22 13:19:59 -0800

    Update footer_html_pilot_24FEB2017.js

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -9,5 +9,4 @@ jQuery(document).ready(function($) {
     }
       console.log("Conflict Free jQuery goes here");
       $('.topBar').css({'backgroundColor': '#004593',  'margin-top': '0px', 'margin-right': '10px', 'border': '0px solid #004593'});  
-    });
-});
+ });
