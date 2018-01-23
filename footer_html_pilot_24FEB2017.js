7e09c9bf8145a6493fd3a02ccfa19706e6a8550e
gcses <john.santucci@carefusion.com>
2018-01-23 14:30:39 -0800

    Update footer_html_pilot_24FEB2017.js

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -24,7 +24,7 @@
 		/***** BEGIN - Navigation *****/
 		
 		$smjquery('.navTabs').css({'background': '#fff' });
-		$smjquery('.navTabs li a').css({'color':'#999', 'text-shadow' : 'none' , 'border-top' : '1px solid #ccc'});
+		$smjquery('.navTabs li a').css({'color':'#5f6062', 'text-shadow' : 'none' , 'border-top' : '1px solid #ccc'});
 		$smjquery('.navTabs li.active').css({'text-decoration' : 'underline', 'color' : '#004593', 'background-color' : 'transparent' , 'border-top' : '1px solid #ccc'});
 		$smjquery('.navTabs li:over').css({'background-color' : 'transparent'});
 		
