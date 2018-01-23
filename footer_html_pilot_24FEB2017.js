a8d479834c0b679c51faf0637fd28d56dd2fc67c
gcses <john.santucci@carefusion.com>
2018-01-23 13:53:32 -0800

    Update footer_html_pilot_24FEB2017.js

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -7,9 +7,9 @@
    
 	$smjquery(document).ready(function() {
 		//overlay a new image to cover up settings cog image
-		var cogImg = $smjquery('<img class="cog1">');
-		cogImg.attr('src', 'https://bd-pilot.csod.com/clientimg/bd/welcome/cog.png');
-		cogImg.appendTo('#optionsbtn');
+		//var cogImg = $smjquery('<img class="cog1">');
+		//cogImg.attr('src', 'https://bd-pilot.csod.com/clientimg/bd/welcome/cog.png');
+		//cogImg.appendTo('#optionsbtn');
 		$smjquery('.cog1').css({'height': '30px', 'position': 'absolute', 'top': '0', 'left': '-1px', 'margin-left': '0px', 'margin-right': '0px', 'border-radius': '0', 'border': '0px', "zindex": "2147483644"});
 		$smjquery('#optionsbtn').css({'padding-left': '5px', 'margin-right': '8px', 'backgroundColor': 'yellow', 'border': '0px', "zindex": "2147483644"});
 
