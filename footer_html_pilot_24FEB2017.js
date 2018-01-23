38ee3091249914c746f187235533f0731079ba0f
gcses <john.santucci@carefusion.com>
2018-01-23 14:41:04 -0800

    Update footer_html_pilot_24FEB2017.js

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -3,7 +3,7 @@
 	$smjquery(document).ready(function() {
 		//overlay a new image to cover up settings cog image
 		//var cogImg = $smjquery('<img class="cog1">');
-		//cogImg.attr('src', 'https://bd-pilot.csod.com/clientimg/bd/welcome/cog.png');
+		//cogImg.attr('src', 'https://bd-pilot.csod.com/clientimg/bd/welcome/cog.png'); /clientimg/bd/welcome/avatar.png
 		//cogImg.appendTo('#optionsbtn');
 		//$smjquery('.cog1').css({'height': '30px', 'position': 'absolute', 'top': '0', 'left': '-1px', 'margin-left': '0px', 'margin-right': '0px', 'border-radius': '0', 'border': '0px', "zindex": "2147483644"});
 		//$smjquery('#optionsbtn').css({'padding-left': '5px', 'margin-right': '8px', 'backgroundColor': 'yellow', 'border': '0px', "zindex": "2147483644"});
@@ -18,8 +18,14 @@
 		/*****BEGIN: Search Bar Customization *****/
 		$smjquery('#search .cso-top-srch-box').css({'backgroundColor': '#fff', 'border-radius': '4px', 'margin-top': '7px', 'margin-left': '-10px', 'margin-right': '10px', 'border': '0px solid #004593' , 'padding-left': '0'});
 		$smjquery('#search input').css({'color':'#ccc'});
-		
 		/*****END: Search Bar Customization *****/
+		
+		/***** BEGIN : AVATAR IMAGE **********/
+		$smjquery('.avatar a img').css({ 'display' : 'none'});
+		var avatargImg = $smjquery('<img class=".avatar a img">');
+		cogImg.attr('src', 'https://bd-pilot.csod.com/clientimg/bd/welcome/avatar.png');
+		$smjquery('.avatar a img').css({ 'height' : '30px' , 'width' : '30px'});
+		/******* END : AVATAR Image *****/ 
 		
 		/***** BEGIN - Navigation *****/
 		
