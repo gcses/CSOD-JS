2013f4e7ae9bdb02e5ced94e220564b65b9d6610
gcses <john.santucci@carefusion.com>
2018-01-23 14:16:25 -0800

    Update footer_html_pilot_24FEB2017.js

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -1,4 +1,43 @@
+(function($) {
+   
+	$smjquery(document).ready(function() {
+		//overlay a new image to cover up settings cog image
+		//var cogImg = $smjquery('<img class="cog1">');
+		//cogImg.attr('src', 'https://bd-pilot.csod.com/clientimg/bd/welcome/cog.png');
+		//cogImg.appendTo('#optionsbtn');
+		//$smjquery('.cog1').css({'height': '30px', 'position': 'absolute', 'top': '0', 'left': '-1px', 'margin-left': '0px', 'margin-right': '0px', 'border-radius': '0', 'border': '0px', "zindex": "2147483644"});
+		//$smjquery('#optionsbtn').css({'padding-left': '5px', 'margin-right': '8px', 'backgroundColor': 'yellow', 'border': '0px', "zindex": "2147483644"});
 
+		$smjquery('.topBar').css({'backgroundColor': '#004593',  'margin-top': '0px', 'margin-right': '10px', 'border': '0px solid #004593'});  //yellow border
+		
+		/***** BEGIN : Header Tools *****/
+		$smjquery('.topBarOuter .tools').css({'padding': '10px', 'height' : '40px'});
+		$smjquery('.topBarOuter .tools span').css({'margin-right': '15px'});
+		
+		
+		/*****BEGIN: Search Bar Customization *****/
+		$smjquery('#search .cso-top-srch-box').css({'backgroundColor': '#fff', 'border-radius': '4px', 'margin-top': '7px', 'margin-left': '-10px', 'margin-right': '10px', 'border': '0px solid #004593' , 'padding-left': '0'});
+		$smjquery('#search input').css({'color':'#ccc'});
+		
+		/*****END: Search Bar Customization *****/
+		
+		/***** BEGIN - Navigation *****/
+		
+		$smjquery('.navTabs').css({'background': '#fff' });
+		$smjquery('.navTabs li a').css({'color':'#999', 'text-shadow' : 'none'});
+		$smjquery('.navTabs li.active').css({'text-decoration' : 'underline', 'color' : '#004593', 'background-color' : 'transparent' , 'border' : '1px solid #ccc'});
+		$smjquery('.navTabs li:hover').css({'background-color' : 'transparent'});
+		
+		/***** END - Navigation *****/
+		
+		$smjquery('.avatar').css({'backgroundColor': '#004593', 'border-radius': '0', 'margin-top': '7px', 'margin-left': '-10px', 'margin-right': '10px', 'border': '0px solid #004593' , 'padding-left': '0'});
+		$smjquery('.avatar a .img').css({'backgroundColor': '#004593', 'border-radius': '0', 'margin-top': '7px', 'margin-left': '-10px', 'border': '0px solid #004593'});
+		$smjquery('.avatar a').css({'backgroundColor': '#004593', 'border-radius': '0',  'margin-top': '0px', 'margin-right': '10px', 'border': '0px solid #004593'});
+		$smjquery('.user-interaction').css({'backgroundColor': '#004593', 'border-radius': '0', 'border': '0px solid #004593'});
 
+		
+		// FOOTER 
+		$smjquery('.df-footer').css({"display": "none"}); // hide the csod default footer
+	});
 
-
+}($smjquery)); 
