4d8e3f353f54faaf9944dde7cd6e9ef986be6491
gcses <john.santucci@carefusion.com>
2018-01-25 17:09:19 -0800

    Update footer_html_pilot_24FEB2017.js

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -54,9 +54,9 @@
 	
 		
 		
-		$('.p-contentslider').after($('<div class="cardscontainer">4 columns go here<div class="col-1-card">1</div></div>'));
+		$smjquery('.p-contentslider').after($smjquery('<div class="cardscontainer">4 columns go here<div class="col-1-card">1</div></div>'));
 		
-		$('div.p-contentslider').after('<div id="space">testtesttest</div>');
+		$smjquery('div.p-contentslider').after('<div id="space">testtesttest</div>');
 		/********BEGIN - Featured Cards Component **********/
 		
 		// FOOTER 
