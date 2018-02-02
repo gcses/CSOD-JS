cb9d617c4d6795b6ae7684bd69bcf5a0c8af8c47
gcses <john.santucci@carefusion.com>
2018-02-02 09:31:05 -0800

    Update footer_html_pilot_24FEB2017.js

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -64,15 +64,15 @@
 		footerContents.appendTo('#ctl00_footer_pnlCustomFooter');
 		
 		 $smjquery('#ctl00_footer_pnlCustomFooter').css({'padding' : '0'});
-		 $smjquery('.footerContentsRow').css({'backgroundColor': '#004593', 'color': '#fff' , 'font-size' : '11px' , 'line-height' : '15px' , 
-'padding' : '20px 0 30px', 'position' : 'relative' , 'text-align' : 'center' ,  'margin-top' : '-5px' , 'height' : '150px'});
+		 $smjquery('.footerContentsRow').css({'backgroundColor': '#004593', 'color': '#fff' , 'font-size' : '11px' ,
+'padding' : '20px', 'text-align' : 'center' ,  'margin-top' : '-5px' , 'height' : '150px'});
 		 $smjquery('.footerContentsRow div.col').css({'float' : 'left', 'display' : 'inline-block', 'min-width' : '40px' , 'width' : 'auto' , 'padding' : '20px' });
 		// now style the footer elements
 		$smjquery('#sm1').css({'height': '40px'});
 		$smjquery('#sm2').css({'height': '45px'});
 		$smjquery('#sm3').css({'height': '37px'});
 		$smjquery('#sm4').css({'height': '37px'});
-		$smjquery('.footerLink').css({'float' : 'right' , 'border-right': '1px solid #ccc' , 'padding-top' : '0' , 'margin-top' : '20px' });
+		$smjquery('.footerLink').css({'float' : 'right' , 'border-right': '1px solid #ccc' , 'padding-top' : '0' , 'padding-bottom' : '0' , 'margin-top' : '20px' });
 		$smjquery('.footerLink a').css({'color' : '#fff'});
 		
 		/***** END - Footer *****/
