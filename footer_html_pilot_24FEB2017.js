65514aada4903c3a75280b2e3e76f4eaa1eda39f
gcses <john.santucci@carefusion.com>
2018-02-05 14:56:14 -0800

    Update footer_html_pilot_24FEB2017.js

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -3,49 +3,47 @@
 	$smjquery(document).ready(function() {
 	
 		/***** BEGIN : Header Tools *****/
-		$smjquery('.topBarOuter .tools').css({'padding': '10px', 'height' : '40px' , 'margin-right' : '20px'});
-		$smjquery('.topBar').css({'backgroundColor': '#023970',  'margin-top': '0px', 'margin-right': '10px', 'border': 'none'}); 
+		$('.topBarOuter .tools').css({'padding': '10px', 'height' : '40px' , 'margin-right' : '20px'});
+		$('.topBar').css({'backgroundColor': '#023970',  'margin-top': '0px', 'margin-right': '10px', 'border': 'none'}); 
 		
 		/*****BEGIN: Search Bar Customization *****/
-		$smjquery('#search .cso-top-srch-box').css({'backgroundColor': '#fff', 'border-radius': '4px', 'margin-top': '7px', 'margin-left': '-10px', 'margin-right': '30px', 'border': '0px solid #004593' , 'padding-left': '0'});
-		$smjquery('#search input').css({'color':'#ccc'});
+		$('#search .cso-top-srch-box').css({'backgroundColor': '#fff', 'border-radius': '4px', 'margin-top': '7px', 'margin-left': '-10px', 'margin-right': '30px', 'border': '0px solid #004593' , 'padding-left': '0'});
+		$('#search input').css({'color':'#ccc'});
 		/*****END: Search Bar Customization *****/
 		
 		/***** BEGIN : AVATAR IMAGE **********/
 		//$smjquery('.avatar a img').css({ 'display' : 'none'});
-		var avatargImg = $smjquery(document.getElementById('ctl00_header_headerFixed_imgAvatar'));
+		var avatargImg = $(document.getElementById('ctl00_header_headerFixed_imgAvatar'));
 		avatargImg.attr('src', 'https://bd-pilot.csod.com/clientimg/bd/welcome/avatar2.png');
-		$smjquery('.user-interaction .avatar a img').css({ 'height' : '40px !important' , 'width' : '40px !important'});
-		$smjquery('.avatar').css({'backgroundColor': '#004593', 'border-radius': '0', 'margin-top': '7px', 'margin-left': '-10px', 'margin-right': '10px', 'border': '0px solid #004593' , 'padding-left': '0'});
-		$smjquery('.avatar a .img').css({'backgroundColor': '#004593', 'border-radius': '0', 'margin-top': '7px', 'margin-left': '-10px', 'border': '0px solid #004593'});
-		$smjquery('.avatar a').css({'backgroundColor': '#004593', 'border-radius': '0',  'margin-top': '0px', 'margin-right': '10px', 'border': '0px solid #004593'});
+		$('.user-interaction .avatar a img').css({ 'height' : '40px !important' , 'width' : '40px !important'});
+		$('.avatar').css({'backgroundColor': '#004593', 'border-radius': '0', 'margin-top': '7px', 'margin-left': '-10px', 'margin-right': '10px', 'border': '0px solid #004593' , 'padding-left': '0'});
+		$('.avatar a .img').css({'backgroundColor': '#004593', 'border-radius': '0', 'margin-top': '7px', 'margin-left': '-10px', 'border': '0px solid #004593'});
+		$('.avatar a').css({'backgroundColor': '#004593', 'border-radius': '0',  'margin-top': '0px', 'margin-right': '10px', 'border': '0px solid #004593'});
 		/******* END : AVATAR Image *****/ 
 		
 		/************ BEGIN : COG ***********/
-		$smjquery('.user-interaction a span').css({'background-image' : 'url("https://bd-pilot.csod.com/clientimg/bd/welcome/gear5.png")' , 'top' : '-1px' , 'left' : '-1px' });
-		$smjquery('.user-interaction').css({'backgroundcolor': 'none', 'border-radius': '0', 'border': 'none'});
+		$('.user-interaction a span').css({'background-image' : 'url("https://bd-pilot.csod.com/clientimg/bd/welcome/gear5.png")' , 'top' : '-1px' , 'left' : '-1px' });
+		$('.user-interaction').css({'backgroundcolor': 'none', 'border-radius': '0', 'border': 'none'});
 		//$smjquery('.user-interaction li').css({'border-radius': '0', 'border': 'none'});
-		$smjquery('.cso-hdr-dd .item a span').css({'background-image' : 'none' });
-		$smjquery('.settings a span').css({'background-position' : '0 0' , 'height' : '40px' , 'width' : '40px' });
-		$smjquery('.cso-hdr-dd .item a span').css({'height' : 'auto' , 'width' : 'auto' });
+		$('.cso-hdr-dd .item a span').css({'background-image' : 'none' });
+		$('.settings a span').css({'background-position' : '0 0' , 'height' : '40px' , 'width' : '40px' });
+		$('.cso-hdr-dd .item a span').css({'height' : 'auto' , 'width' : 'auto' });
 
 		/***** BEGIN - Navigation *****/
-		$smjquery('.navTabs').css({'background': '#023970' });
-		$smjquery('.navTabs li').css({'border-right': '1px solid #444c54' });
-		$smjquery('.navTabs-subTabList li').css({'border-right': 'none' });
-		$smjquery('.navTabs li:last-child').css({'border-right': 'none' });
-		$smjquery('.navTabs li a').css({'color':'#fff', 'text-shadow' : 'none'});
-		$smjquery('.navTabs li a:hover').css({'text-decoration' : 'underline', 'text-shadow' : 'none', 'opacity' : '0.7'});
-		$smjquery('.navTabs li.active').css({'text-decoration' : 'underline' , 'font-weight' : 'bold'});
-		$smjquery('.c-glbl-nav').css({'border-top' : '1px solid #545d6d'});
+		$('.navTabs').css({'background': '#023970' });
+		$('.navTabs li').css({'border-right': '1px solid #444c54' });
+		$('.navTabs-subTabList li').css({'border-right': 'none' });
+		$('.navTabs li:last-child').css({'border-right': 'none' });
+		$('.navTabs li a').css({'color':'#fff', 'text-shadow' : 'none'});
+		$('.navTabs li a:hover').css({'text-decoration' : 'underline', 'text-shadow' : 'none', 'opacity' : '0.7'});
+		$('.navTabs li.active').css({'text-decoration' : 'underline' , 'font-weight' : 'bold'});
+		$('.c-glbl-nav').css({'border-top' : '1px solid #545d6d'});
 		/***** END - Navigation *****/
-
-
 		
 		/***** BEGIN - Footer *****/
-		$smjquery('.df-footer').css({"display": "none"}); // hide the csod default footer
+		$('.df-footer').css({"display": "none"}); // hide the csod default footer
 		//create grey background and add sloped image
-		$smjquery('#ctl00_footer_pnlCustomFooter').css({"backgroundColor": "#f2f2f2"});
+		$('#ctl00_footer_pnlCustomFooter').css({"backgroundColor": "#f2f2f2"});
 		var footerImg = $('<img id="slopedFooterImage">');
 	        footerImg.attr('src', 'https://bd-pilot.csod.com/clientimg/bd/welcome/angle.png');
                 footerImg.appendTo('#ctl00_footer_pnlCustomFooter');
@@ -62,21 +60,21 @@
 			'</div>');
 		
 		footerContents.appendTo('#ctl00_footer_pnlCustomFooter');
-		$smjquery('#slopedFooterImage').css({'max-width' : '100%', 'display' : 'block' , 'height' : 'auto'});
-		$smjquery('#ctl00_footer_pnlCustomFooter').css({'padding' : '0'});
-		$smjquery('.footerContentsRow').css({'backgroundColor': '#004593', 'color': '#fff' , 'font-size' : '11px' ,
+		$('#slopedFooterImage').css({'max-width' : '100%', 'display' : 'block' , 'height' : 'auto'});
+		$('#ctl00_footer_pnlCustomFooter').css({'padding' : '0'});
+		$('.footerContentsRow').css({'backgroundColor': '#004593', 'color': '#fff' , 'font-size' : '11px' ,
 'padding' : '30px 20px 20px', 'text-align' : 'center' ,  'margin-top' : '-5px' , 'height' : '150px'});
-		$smjquery('.footerContentsRow div.col').css({'float' : 'left', 'display' : 'inline-block', 'min-width' : '20px' , 'width' : 'auto' , 'padding-top' : '20px' });
-		$smjquery('.footerContentsRow div.copyright').css({'width' : '50%'});
+		$('.footerContentsRow div.col').css({'float' : 'left', 'display' : 'inline-block', 'min-width' : '20px' , 'width' : 'auto' , 'padding-top' : '20px' });
+		$('.footerContentsRow div.copyright').css({'width' : '50%'});
 		// now style the footer elements
-		$smjquery('#footerLogo').css({'position' : 'relative' , 'top': '-35px' });
-		$smjquery('#sm1').css({'height': '20px'});
-		$smjquery('#sm2').css({'height': '27px'});
-		$smjquery('#sm3').css({'height': '25px'});
-		$smjquery('#sm4').css({'height': '25px'});
-		$smjquery('.footerLink').css({'float' : 'right' , 'border-right': '1px solid #ccc' , 'padding-top' : '0' , 'padding-bottom' : '0' , 'margin-top' : '20px' });
-		$smjquery('.footerLink:first-child').css({'border-right' : '0'});
-		$smjquery('.footerLink a').css({'color' : '#fff'});
+		$('#footerLogo').css({'position' : 'relative' , 'top': '-35px' });
+		$('#sm1').css({'height': '20px'});
+		$('#sm2').css({'height': '27px'});
+		$('#sm3').css({'height': '25px'});
+		$('#sm4').css({'height': '25px'});
+		$('.footerLink').css({'float' : 'right' , 'border-right': '1px solid #ccc' , 'padding-top' : '0' , 'padding-bottom' : '0' , 'margin-top' : '20px' });
+		$('.footerLink:first-child').css({'border-right' : '0'});
+		$('.footerLink a').css({'color' : '#fff'});
 		
 		/***** END - Footer *****/
 	});
