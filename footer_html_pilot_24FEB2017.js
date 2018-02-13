2bf8725ec36c405e8227d9016a36eae0999c5e55
gcses <john.santucci@carefusion.com>
2018-02-13 12:29:04 -0800

    Update footer_html_pilot_24FEB2017.js

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -31,7 +31,7 @@
 		$('.cso-hdr-dd .item a span').css({'height' : 'auto' , 'width' : 'auto' });
 
 		/***** BEGIN - Navigation *****/
-		$('<span class=\"lc-title\">Learning Compass</span>').insertBefore('.c-glbl-nav ul li:first-child');
+		$('<span class=\"lc-title\">Learning Compass</span>').insertBefore('.c-glbl-nav ul:first-child li:first-child');
 		$('.navTabs').css({'background': '#fff' });
 		$('.navTabs li').css({'border-right': '1px solid #444c54' });
 		$('.navTabs-subTabList li').css({'border-right': 'none' });
