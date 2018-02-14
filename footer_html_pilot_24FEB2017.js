3689ac1eddea274ab5906ab1b436cbcc85636135
gcses <john.santucci@carefusion.com>
2018-02-14 15:27:00 -0800

    Update footer_html_pilot_24FEB2017.js

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -13,7 +13,7 @@
 		/*****BEGIN: Search Bar Customization *****/
 		$('#search').css({ 'border-right': '1px solid #1a589e'});
 		$('#search .cso-top-srch-box').css({'width' : '140px' , 'backgroundColor': '#fff', 'border-radius': '4px', 'margin-right': '20px' , 'border' : 'none' , 'padding-left': '0'});
-		$('#search input').css({'color':'#ccc' , 'width' : '130px' });
+		$('#search input').css({'color':'#808080' , 'width' : '130px' });
 		$('#search .search-lnk').css({'background-image' : 'url("https://bd-pilot.csod.com/clientimg/bd/welcome/magnifyingGlass.png")' , 'background-position' : '-5px -4px' });
 		
 		/*****END: Search Bar Customization *****/
