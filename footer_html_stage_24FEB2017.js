36d1b074f931f18d2ff1ecea9d5277ff97fd57e8
gcses <john.santucci@carefusion.com>
2018-09-14 11:26:03 -0700

    Update footer_html_stage_24FEB2017.js

--- footer_html_stage_24FEB2017.js
+++ footer_html_stage_24FEB2017.js
@@ -77,7 +77,7 @@
 		//create grey background and add sloped image
 		$('#ctl00_footer_pnlCustomFooter').css({"backgroundColor": "#e6e7e9"});
 		
-		var footerQuickLinksBox = $('<div id="footerQuickLinks" style="height : auto; background-color: #d9d9d9;">' +
+		var footerQuickLinksBox = $('<div id="footerQuickLinks" style="height : auto; background-color: #d9d9d9; float: left;">' +
 			'<div class="row">' +
 					    '<div class="col"><p>Hot Topics</p>' +
 					    	'<ul>' +
