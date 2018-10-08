42ed52fa27e190a36315c403226cd6bd58df8df8
gcses <john.santucci@carefusion.com>
2018-10-08 11:19:37 -0700

    Update footer_html_stage_24FEB2017.js

--- footer_html_stage_24FEB2017.js
+++ footer_html_stage_24FEB2017.js
@@ -51,7 +51,7 @@
 		lcTitleImg.attr('src', 'https://bd-stg.csod.com/clientimg/bd/welcome/Learning_Compass_title_img1.png');
 		$(lcTitleImg).insertBefore('.c-glbl-nav ul:first-child');
 		$('#lcTitle').css({'float': 'left', 'height' : '40px' });
-		$('.navTabs').css({'background': '#fff' , 'padding-top' : '12px' });
+		$('.navTabs').css({'background': '#fff' , 'padding-top' : '1px' });
 		$('.navTabs li').css({'padding-right': '5px'});
 		$('.navTabs-subTabList li').css({'border-right': 'none' });
 		$('.navTabs li:last-child').css({'border-right': 'none' });
