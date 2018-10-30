33fe43cb763bec991bc86ea0c401e9d8267babaa
gcses <john.santucci@carefusion.com>
2018-10-30 10:54:25 -0700

    Update footer_html_pilot_24FEB2017.js

--- footer_html_pilot_24FEB2017.js
+++ footer_html_pilot_24FEB2017.js
@@ -7,8 +7,11 @@
 		$('.topBarOuter .tools').css({'padding': '0 10px', 'height' : '60px' , 'margin-right' : '20px'});
 		$('.topBar').css({'margin-top': '0px', 'margin-right': '10px', 'border': 'none'}); 
 		
+		$('div[role=navigation]').css ({ 'border-bottom' : '1px solid #b6b6b6'});
+
+		
 		var feedback_button = $('<img id=\"fdSurvey\">');
-		feedback_button.attr('src', 'https://bd-pilot.csod.com/clientimg/bd/welcome/btn_feedback.png');
+		feedback_button.attr('src', 'https://bd.csod.com/clientimg/bd/welcome/btn_feedback.png');
 		$(feedback_button).insertBefore('#search');
 		$(feedback_button).css({'width' : '80px' , 'height' : '35px' ,  'margin-right': '20px', 'cursor' : 'pointer'});
 		$(feedback_button).wrap($("<a class=\"feedback-link\">").attr('href', 'https://bd2.az1.qualtrics.com/jfe/form/SV_5yWSp8x3k8D4PBz'));
@@ -21,13 +24,13 @@
 		$('#search').css({ 'border-right': '1px solid #1a589e'});
 		$('#search .cso-top-srch-box').css({'width' : '140px' , 'backgroundColor': '#fff', 'border-radius': '4px', 'margin-right': '20px' , 'border' : 'none' , 'padding-left': '0'});
 		$('#search input').css({'color':'#808080' , 'width' : '130px' });
-		$('#search .search-lnk').css({'background-image' : 'url("https://bd-pilot.csod.com/clientimg/bd/welcome/magnifyingGlass.png")' , 'background-position' : '-5px -4px' });
+		$('#search .search-lnk').css({'background-image' : 'url("https://bd.csod.com/clientimg/bd/welcome/magnifyingGlass.png")' , 'background-position' : '-4px -2px' });
 		
 		/*****END: Search Bar Customization *****/
 		
 		/***** BEGIN : AVATAR IMAGE **********/
 		//var avatargImg = $(document.getElementById('ctl00_header_headerFixed_imgAvatar'));
-		//avatargImg.attr('src', 'https://bd-pilot.csod.com/clientimg/bd/welcome/avatar-sm.png');
+		//avatargImg.attr('src', 'https://bd.csod.com/clientimg/bd/welcome/avatar-sm.png');
 		//$('.user-interaction .avatar a img').css({ 'height' : '40px !important' , 'width' : '40px !important'});
 		$('.avatar').css({'backgroundColor': '#004593', 'margin-right': '10px', 'border': '0px solid #004593' , 'border-radius': '0', 'padding-left': '0'});
 		$('.avatar a .img').css({'backgroundColor': '#004593', 'border-radius': '0', 'border': '0px solid #004593'});
@@ -47,10 +50,10 @@
 		
 		//$('.c-glbl-nav').append("<style>.clearfix::after{ content:'.' ;  visibility: hidden ; display : block ; height : 0 ; clear : both;  }</style>");
 		var lcTitleImg = $('<img id=\"lcTitle\">');
-		lcTitleImg.attr('src', 'https://bd-stg.csod.com/clientimg/bd/welcome/Learning_Compass_title_img1.png');
+		lcTitleImg.attr('src', 'https://bd.csod.com/clientimg/bd/welcome/Learning_Compass_title_img1.png');
 		$(lcTitleImg).insertBefore('.c-glbl-nav ul:first-child');
 		$('#lcTitle').css({'float': 'left', 'height' : '40px' });
-		$('.navTabs').css({'background': '#fff' , 'padding-top' : '1px' });
+		$('.navTabs').css({'background': '#fff'});
 		$('.navTabs li').css({'padding-right': '5px'});
 		$('.navTabs-subTabList li').css({'border-right': 'none' });
 		$('.navTabs li:last-child').css({'border-right': 'none' });
@@ -60,12 +63,20 @@
 		//$('#main-menu li a.has-submenu.highlighted').attr('style', 'background-color: #fff !important');
 		$('.navTabs li.active').css({ 'font-weight' : 'bold !important' ,  'background-color' : '#fff !important' });
 		$('.navTabs li').removeClass('highlighted');
-		$('.navTabs li').hover(function() {
-			$(this).removeClass('highlighted');
-			$(this).css({'background-color': '#fff !important' , 'font-color' : '#5f6062'  });
-		    }, function() {
-			$(this).css({'background-color': '#fff' ,  'font-color' : '#808080'});
-		    });
+		//$('.navTabs li a').hover(function() {
+		//	$(this).removeClass('highlighted');
+		//	$(this).css({'background-color': '#fff !important' , 'font-color' : '#5f6062', 'padding-right' : '0'  });
+		//    }, function() {
+		//	$(this).css({'background-color': '#fff' ,  'font-color' : '#808080'});
+		//    });
+		
+		//$('.navTabs li a').hover(function() {
+		//	$(this).css({'opacity' : '.5'});
+		//    }, function() {
+		//	$(this).css({'opacity' : '.5'});
+		  //  });
+		
+
 		
 		/***** END - Navigation *****/
 		
@@ -77,7 +88,7 @@
 		//create grey background and add sloped image
 		$('#ctl00_footer_pnlCustomFooter').css({"backgroundColor": "#e6e7e9" , "padding" : "0"});
 		
-		var footerQuickLinksBox = $('<div id="footerQuickLinks" style="height : auto; z-index: 1; padding: 30px 0 15px; position: relative;  background-color: #fff; border-top: 2px solid #e5e5e5; float: left; width: 100%;">' +
+		var footerQuickLinksBox = $('<div id="footerQuickLinks" style="height : auto; z-index: 1; padding: 30px 0 15px; border-top: 2px solid #e5e5e5; float: left; width: 100%; background-color: #f5f5f5;">' +
 			'<div class="row">' +
 					    '<div class="col">' +
 					    		'<p>NURSES AND CLINICIANS - QUICK LINKS</p>' +
@@ -93,7 +104,7 @@
 					     '</div>' +
 					     '<div class="col">' +
 					    		'<p>PHARMACY - QUICK LINKS</p>' +
-					    		'<div class="text-link"><a href="https://bd.csod.com/LMS/LoDetails/DetailsLo.aspx?loId=7d262bbf-47db-410c-a205-d2462ae6118a">Preparing for Alaris™ Interoperability - Adobe Connect Webinar - CLP1266</a></div>' +
+					    		'<div class="text-link"><a href="https://bd.csod.com/LMS/LoDetails/DetailsLo.aspx?loId=7d262bbf-47db-410c-a205-d2462ae6118a">Preparing for Alaris&trade; Interoperability - Adobe Connect Webinar - CLP1266</a></div>' +
 							'<div class="text-link"><a href="https://bd.csod.com/LMS/LoDetails/DetailsLo.aspx?loid=2507553d-61bb-49bf-9f9c-9aa6d5a6bb27">CLP1043 - Pyxis MedStation 4000 system for Pharmacy</a></div>' +
 					    		'<div class="text-link"><a href="https://bd.csod.com/LMS/LoDetails/DetailsLo.aspx?loid=ecd49018-3bdf-4cf5-bc68-b87852744ea5">Pyxis MedStation 4000 for System Managers (CLP1008 - CLP1012)</a></div>' +
 							'<div class="text-link"><a href="https://bd.csod.com/LMS/LoDetails/DetailsLo.aspx?loid=b0ce2671-4467-4aec-9308-1af879b6d354">CLP1107 - Pyxis MedStation ES system for Pharmacy, v1.3.4</a></div>' +
@@ -110,7 +121,7 @@
 							'<div class="text-link"><a href="https://bd.csod.com/phnx/driver.aspx?routename=Social/Topic/Posting/QAPostingDetails&Posting=17&Root=-1">How do I view transcripts and/or assign courses in Learning Compass?</a></div>' +
 							'<div class="text-link"><a href="https://bd.csod.com/phnx/driver.aspx?routename=Social/Topic/TopicDetails&Topic=3&Root=-1">Frequently Asked Questions</a></div>' +
 					      '</div>' +
-					    '<div class="col">' +
+					    '<div class="col last-col">' +
 					    		'<p>NEED HELP?</p>' +
 							//'<div class="text-link"><a href="https://bd.csod.com/LMS/LoDetails/DetailsLo.aspx?loid=e049e85e-c131-46a3-90aa-fe2fa5520192#t=1">NEW! - Try our Curriculum Finder to find courses for your role</a></div>' +
 							'<div class="text-link"><a href="https://bd.csod.com/catalog/CustomPage.aspx?id=20000480&tab_page_id=20000480">&#9990; Contact Us</a></div>' +
@@ -118,9 +129,10 @@
 					    '</div>' +
 			 '</div>');
                 footerQuickLinksBox.appendTo('#ctl00_footer_pnlCustomFooter');
-		$('#footerQuickLinks .row').css({'height' : 'auto' , 'width' :  '100%', 'font-size' : '11px',  'line-height' : '1.2', 'display' : 'flex' , 'justify-content' : 'center'});
+		$('#footerQuickLinks .row').css({'height' : 'auto' , 'width' :  '80%', 'font-size' : '11px',  'line-height' : '1.2', 'display' : 'flex' , 'margin' : '0 auto' , 'padding-bottom' : '30px'});
 		$('#footerQuickLinks .row:after').css({'content':'' , 'display' : 'table' , 'clear' :  'both'});
-		$('#footerQuickLinks .col').css({'height' : 'auto' , 'width' :  '28%', 'float' : 'left' , 'padding' : '20px 5px 50px 55px'});
+		$('#footerQuickLinks .col').css({'height' : 'auto' , 'width' :  '28%', 'float' : 'left' , 'padding-right' : '40px'});
+		$('#footerQuickLinks .last-col').css({'padding-right' : '0'});
 		$('#footerQuickLinks .col p').css({'font-size' : '12px' , 'font-weight' :  'bold' , 'color' :  '#444444', 'padding' : '5px 0'});
 		$('#footerQuickLinks .text-link a').css({'font-weight' : 'normal' , 'color' :  '#666666', 'display' : 'block' , 'margin' : '8px 0'});
 		$('#footerQuickLinks div a:hover').css({'font-weight' : 'bold' ,   'opacity' : '0.6' });
@@ -135,15 +147,15 @@
 		    });
 
 		var footerImg = $('<img id="slopedFooterImage">');
-	        footerImg.attr('src', 'https://bd-pilot.csod.com/clientimg/bd/welcome/angle.png');
+	        footerImg.attr('src', 'https://bd.csod.com/clientimg/bd/welcome/angle.png');
                 footerImg.appendTo('#ctl00_footer_pnlCustomFooter');
 		
 		var footerContents = $('<div class="footerContentsRow">' +
 			    '<div class="colX"><img src="https://bd-pilot.csod.com/clientimg/bd/welcome/bd_white_rgb_tag_EN_1_5.png" id="footerLogo"></div>' +
-			    '<div class="col"><a href="http://www.facebook.com/carefusion"><img src="https://bd-pilot.csod.com/clientimg/bd/welcome/facebookIcon.png" id="sm1"></a></div>' +
-			    '<div class="col"><a href="http://www.linkedin.com/company/carefusion"><img src="https://bd-pilot.csod.com/clientimg/bd/welcome/linkedInIcon.jpg" id="sm2"></a></div>' +
-			    '<div class="col"><a href="http://youtube.com/carefusion"><img src="https://bd-pilot.csod.com/clientimg/bd/welcome/youTubeIcon.jpg" id="sm3"></a></div>' +
-			    '<div class="col"><a href="http://twitter.com/carefusion"><img src="https://bd-pilot.csod.com/clientimg/bd/welcome/twitterIcon.jpg" id="sm4"></a> </div>' +
+			    '<div class="col"><a href="http://www.facebook.com/carefusion"><img src="https://bd.csod.com/clientimg/bd/welcome/facebookIcon.png" id="sm1"></a></div>' +
+			    '<div class="col"><a href="http://www.linkedin.com/company/carefusion"><img src="https://bd.csod.com/clientimg/bd/welcome/linkedInIcon.jpg" id="sm2"></a></div>' +
+			    '<div class="col"><a href="http://youtube.com/carefusion"><img src="https://bd.csod.com/clientimg/bd/welcome/youTubeIcon.jpg" id="sm3"></a></div>' +
+			    '<div class="col"><a href="http://twitter.com/carefusion"><img src="https://bd.csod.com/clientimg/bd/welcome/twitterIcon.jpg" id="sm4"></a> </div>' +
 			    '<div class="col copyright"><p class="copy">&copy; 2018 BD. All rights reserved. Unless otherwise noted, BD, the BD Logo and all other trademarks are property of Becton, Dickinson and Company.</p></div>' +
 			    '<div class="col footerLink privacy"><a href="http://www.bd.com/legal/privacy_policy/">Privacy Policy</a></div>' +
 			    '<div class="col footerLink"><a href="http://www.bd.com/legal/terms/">Terms of Use</a></div>' +
@@ -168,8 +180,8 @@
 		$('#sm2').css({'height': '27px'});
 		$('#sm3').css({'height': '25px'});
 		$('#sm4').css({'height': '25px'});
-		$('.footerLink').css({'border-left': '1px solid #ccc' , 'padding' : '0 15px', 'margin-top' : '20px' });
-		$('.privacy').css({'border-left' : 'none'});
+		$('.footerLink').css({'float' : 'right','border-right': '1px solid #ccc' , 'padding' : '10px 15px', 'margin-top' : '20px' });
+		$('.privacy').css({'border-right' : 'none'});
 		$('.footerLink a').css({'color' : '#fff'});
 		
 		/***** END - Footer *****/
