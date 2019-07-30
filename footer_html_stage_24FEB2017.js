0eea5e5b9a529588029a0ab2e185a731c092f052
gcses <john.santucci@carefusion.com>
2019-07-30 11:52:17 -0700

    Update footer_html_stage_24FEB2017.js

--- footer_html_stage_24FEB2017.js
+++ footer_html_stage_24FEB2017.js
@@ -13,11 +13,11 @@
 		/*****BEGIN: Search Bar Customization *****/
 		var homepage = window.location.href.indexOf('?home=') > 0;
 		 if (homepage ){
-     $('#Search').css({ 'display' : 'none'});
-    }
-    else { //<-- Here
-      $('#Search').css({ 'display' : 'block'});
-    }
+		     $('#search').css({ 'display' : 'none'});
+		    }
+		    else {
+		      $('#search').css({ 'display' : 'block'});
+		    }
 		//$('#search').css({ 'border-right': '1px solid #1a589e'});
 		//$('#search .cso-top-srch-box').css({'width' : '140px' , 'backgroundColor': '#fff', 'border-radius': '4px', 'margin-right': '20px' , 'border' : 'none' , 'padding-left': '0'});
 		//$('#search input').css({'color':'#808080' , 'width' : '130px' });
